da53958f811ad96c33d06c2c76cf1be8ab003be6
WaterAthiwongsawat <6310210449@psu.ac.th>
2023-07-13 20:50:54 +0700

    lab2 get all , create

--- src/main/java/com/example/demo/BackendJavaApplication.java
+++ src/main/java/com/example/demo/BackendJavaApplication.java
@@ -8,7 +8,9 @@ public class BackendJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendJavaApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello spring");
+		System.out.println("Spring boot");
+		
 	}
 
 }
