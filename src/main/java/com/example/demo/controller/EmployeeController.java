df22c2d407a3bce18d96c0f8a2bb627849d4356f
WaterAthiwongsawat <6310210449@psu.ac.th>
2023-07-19 12:35:38 +0700

    product

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Employee;
@@ -22,7 +25,8 @@ public class EmployeeController {
 	
 	@PostMapping("/employee")
 	public Employee addEmployee(@RequestBody Employee body) {
-	    
+	
+		
 		for(int i = 0; i < data.size(); i++ ) {
 		  if(data.get(i).getEmployeeId() == body.getEmployeeId()) {
 			  		return null;
@@ -33,4 +37,42 @@ public class EmployeeController {
 		return body;
 	}
 	
+	@GetMapping("/employee/{employeeId}")
+	public Employee getEmployeeDetail(@PathVariable Integer employeeId) {
+		System.out.println("employeeId = "+ employeeId);
+
+
+		for(int i = 0; i < data.size(); i++ ) {
+			  if(employeeId == data.get(i).getEmployeeId()) {
+				  		return data.get(i);
+	}
 }
+		return null;
+}
+	@PutMapping("employee/{employeeId}")
+	public Employee updateEmployee(@PathVariable Integer employeeId,@RequestBody Employee body) {
+		
+		for(int i = 0; i < data.size(); i++ ) {
+			if(employeeId == data.get(i).getEmployeeId()) {
+				 
+		     data.get(i).setFirstName(body.getFirstName());
+		     data.get(i).setLastName(body.getLastName());
+		     data.get(i).setSalary(body.getSalary());
+		     return data.get(i); 
+			}		  
+	}
+		return null;
+	}
+
+	@DeleteMapping("employee/{employeeId}")
+	public String deleteEmployee(@PathVariable Integer employeeId) {
+		for(int i = 0; i < data.size(); i++ ) {
+			  if(employeeId == data.get(i).getEmployeeId()) {
+				  data.remove(i);
+				  return "delete success";
+			  }
+	}
+		return "employee not found";
+	}
+}
+
